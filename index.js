1eb3c5d47e6d5a71ec96de534f09c4d85dcc6a3f
Tim Herker <113173499+ti-he@users.noreply.github.com>
2024-04-28 22:07:39 +0200

    read config from env variable instead of package.json

--- index.js
+++ index.js
@@ -1,45 +1,30 @@
-
-const { existsSync, writeFileSync } = require('fs');
-const { dirname, resolve } = require('path');
-const { sync: mkdirp } = require('mkdirp');
-const { ESLint } = require('eslint');
+const { writeFileSync } = require("fs");
+const { dirname, resolve } = require("path");
+const { sync: mkdirp } = require("mkdirp");
+const { ESLint } = require("eslint");
 
 const eslint = new ESLint();
 
-function findConfig () {
-  let root = resolve(process.cwd());
-  while (existsSync(root + '/package.json')) {
-    const config = require(root + '/package.json');
-    if (config['eslint-formatter-multiple']) {
-      return config['eslint-formatter-multiple'];
-    }
-    root = resolve(root + '/../');
-  }
-  return null;
-}
-
-module.exports = async function (results, args) {
-  const root = resolve(process.cwd());
-  const config = findConfig();
+module.exports = async function (results) {
+  const config = JSON.parse(process.env.ESLINT_MULTI_FORMATTER_CONFIG);
 
   if (!config) {
-    console.error('Unable to find a package config that has eslint-formatter-multiple. See README');
+    console.error("Unable to find a config for eslint-formatter-multiple.");
     return false;
   }
 
   for (const formatterConfig of config.formatters || []) {
     const formatter = await eslint.loadFormatter(formatterConfig.name);
     const formatterResult = formatter.format(results);
-    if (formatterConfig.output === 'console') {
+    if (formatterConfig.output === "console") {
       console.log(formatterResult);
-    } else if (formatterConfig.output === 'file') {
-      const filePath = resolve(root, formatterConfig.path);
+    } else if (formatterConfig.output === "file") {
+      const filePath = resolve(process.cwd(), formatterConfig.path);
       try {
         mkdirp(dirname(filePath));
         writeFileSync(filePath, formatterResult);
       } catch (ex) {
-        console.error('There was a problem writing the output file:\n%s', ex);
-
+        console.error("There was a problem writing the output file:\n%s", ex);
         return false;
       }
     }
